cd4de885d44186056356f4633ebe42adcaf87e7f
giahuyduongdev <giahuyduongdev@gmail.com>
2025-08-28 10:59:06 +0700

    feat: Implement MongoDB integration and connection setup
    - Add `mongoose` dependency for database interactions.
    - Introduce `src/dbs/init.mongodb.js` and `src/dbs/init.mongodb.lv0.js` to manage MongoDB connections.
    - Create `src/helpers/check.connect.js` for monitoring database connection status.
    - Update `src/app.js` to utilize the new database connection logic.

---
+++ src/dbs/init.mongodb.lv0.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+
+const connectString = `mongodb://root:123456@localhost:27018/`;
+mongoose.connect(connectString).then(_ => console.log(`Connected Mongodb Success`))
+  .catch(err => console.log(`Error Connect!`))
+
+//dev
+if (1 === 1) {
+  mongoose.set('debug', true);
+  mongoose.set('debug', { color: true });
+}
+
+module.exports = mongoose;
